75afce7944f6c1e80df5d44266be0b59be163174
dchizhikov <dchizhikov@gmail.com>
2024-12-06 07:35:13 +0000

    webApp_Auth 0.4 - from modules.utils

--- modules/code_my.py
+++ modules/code_my.py
@@ -1,18 +1,9 @@
-import io
-import json
-import os
-import requests
 import sys
-
-from flask import Flask, render_template_string, request, Response, session
-from PIL import Image
+from flask import Flask, render_template_string, request, session
+from modules.utils import get_auth_form, get_favicon, load_html_from_file, welcome_page 
 
 # Прокси-порт для доступа к приложению
 base_url = sys.argv[3]
-
-def load_html_from_file(filename):
-  with open(filename, 'r', encoding='utf-8') as file:
-      return file.read()
 
 def run_app():
   app = Flask(__name__)
@@ -25,17 +16,6 @@ def run_app():
   }
   html_file = '/content/webApp_Auth/html/base_template.html'
   base_template = load_html_from_file(html_file)
-
-  def get_favicon():
-    # Создаем изображение 16x16 пикселей
-    img = Image.new('RGB', (16, 16), color='blue')  # Пример: синяя иконка
-    img_bytes = io.BytesIO()
-
-    # Сохраняем изображение в формате ICO
-    img.save(img_bytes, format='ICO')
-    img_bytes.seek(0)  
-    return Response(img_bytes.getvalue(), mimetype='image/x-icon')
-#'''
 
   @app.route('/favicon.ico')
   def favicon():
@@ -64,25 +44,6 @@ def run_app():
       title = "Авторизация"
       content = get_auth_form(error)
       return render_template_string(base_template.format(title=title, content=content))
-
-  def get_auth_form(error=None):
-    # Путь к HTML-файлу
-    html_file = '/content/webApp_Auth/html/auth_form.html'
-
-    # Загрузка HTML-кода из файла
-    form_html = load_html_from_file(html_file)
-
-    # Заменяем плейсхолдер на сообщение об ошибке
-    error_html = f"<p style='color: red;'>{error}</p>" if error else "<p> </p>"
-    form_html = form_html.replace("{error_html}", error_html)
-
-    return form_html
-
-  def welcome_page():
-      if 'username' in session:
-          return f"<h1>Добро пожаловать, {session['username']}!</h1><a href='/logout'>Выйти</a>"
-      else:
-          return "Ошибка: вы не авторизованы.", 403
 
   @app.route('/logout')
   def logout():
---
+++ modules/utils.py
@@ -0,0 +1,37 @@
+import io
+from flask import Response, session
+from PIL import Image
+
+def load_html_from_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def get_favicon():
+    # Создаем изображение 16x16 пикселей
+    img = Image.new('RGB', (16, 16), color='blue')  # Пример: синяя иконка
+    img_bytes = io.BytesIO()
+
+    # Сохраняем изображение в формате ICO
+    img.save(img_bytes, format='ICO')
+    img_bytes.seek(0)  
+    return Response(img_bytes.getvalue(), mimetype='image/x-icon')
+
+def get_auth_form(error=None):
+  # Путь к HTML-файлу
+  html_file = '/content/webApp_Auth/html/auth_form.html'
+
+  # Загрузка HTML-кода из файла
+  form_html = load_html_from_file(html_file)
+
+  # Заменяем плейсхолдер на сообщение об ошибке
+  error_html = f"<p style='color: red;'>{error}</p>" if error else "<p> </p>"
+  form_html = form_html.replace("{error_html}", error_html)
+
+  return form_html
+
+def welcome_page():
+    if 'username' in session:
+        return f"<h1>Добро пожаловать, {session['username']}!</h1><a href='/logout'>Выйти</a>"
+    else:
+        return "Ошибка: вы не авторизованы.", 403
+
